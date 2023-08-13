371b6ddc719c0dd5ddea12c1303411e627870770
Michael Young <michael25young25@gmail.com>
2023-08-13 12:26:01 -0400

    date

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,13 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      fontFamily: {
+        showtime: ['var(--font-showtime)'],
+      },
+      colors: {
+        'red': 'rgb(153, 27, 30)',
+        'gold': 'rgb(255, 204, 0)',
+      }
     },
   },
   plugins: [],
