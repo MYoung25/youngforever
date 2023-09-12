48c655c4d7a4ad44ee44c5f4009b0787e9d73aa7
Michael Young <michael25young25@gmail.com>
2023-09-11 20:58:37 -0400

    add scroll prevention to navigation modal

--- src/app/navigationSidebar.tsx
+++ src/app/navigationSidebar.tsx
@@ -15,20 +15,45 @@ type Props = {
 
 export const NavigationSidebar = (props: Props) => {
   const [isLoaded, setIsLoaded] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
   const modalRef = useRef<HTMLDialogElement>(null);
 
   useEffect(() => {
     setIsLoaded(true);
   }, []);
 
+  /**
+   * Close modal when user clicks outside of it
+   */
+  useEffect(() => {
+    if (modalRef) {
+      modalRef.current?.on("close", () => {
+        setIsOpen(false);
+      });
+    }
+  }, [modalRef]);
+
+  /**
+   * Add overflow-hidden to body when modal is open
+   */
+  useEffect(() => {
+    if (isOpen) {
+      document.body.classList.add("overflow-hidden");
+    } else {
+      document.body.classList.remove("overflow-hidden");
+    }
+  }, [isOpen]);
+
+  function openDialog() {
+    setIsOpen(true);
+    modalRef.current?.showModal();
+  }
+
   if (!isLoaded) return <></>;
 
   return (
     <>
-      <button
-        className="sm:block md:hidden"
-        onClick={() => modalRef.current?.showModal()}
-      >
+      <button className="sm:block md:hidden" onClick={openDialog}>
         <svg height={40} width={40} className="mr-4">
           {[0, 1, 2].map((i) => (
             <rect
@@ -43,26 +68,32 @@ export const NavigationSidebar = (props: Props) => {
       </button>
 
       {createPortal(
-        <dialog
-          ref={modalRef}
-          className="backdrop:backdrop-blur-sm open:bg-red w-60 fixed py-5 top-0 right-0 z-40 rounded-md border-4 border-gold border-double"
-          onClick={() => modalRef.current?.close()}
-        >
-          <div>
-            <ul className="flex-col justify-evenly gap-4 md:flex flex-1 text-center text-gold">
-              {props.menuItems.map((item) => (
-                <li key={item.path} className="my-3">
-                  <Link
-                    href={item.path}
-                    className="font-debonair text-gold text-3xl hover:text-white"
-                  >
-                    {item.name}
-                  </Link>
-                </li>
-              ))}
-            </ul>
-          </div>
-        </dialog>,
+        <>
+          {/* {modalRef.current?.open && (
+            <div className="h-screen w-screen fixed top-0 left-0 z-50" />
+          )} */}
+          <dialog
+            ref={modalRef}
+            className="backdrop:backdrop-blur-sm backdrop:overscroll-contain open:bg-red w-60 fixed py-5 top-0 right-0 z-40 rounded-md border-4 border-gold border-double"
+            onClick={() => modalRef.current?.close()}
+            onClose={() => setIsOpen(false)}
+          >
+            <div>
+              <ul className="flex-col justify-evenly gap-4 md:flex flex-1 text-center text-gold">
+                {props.menuItems.map((item) => (
+                  <li key={item.path} className="my-3">
+                    <Link
+                      href={item.path}
+                      className="font-debonair text-gold text-3xl hover:text-white"
+                    >
+                      {item.name}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </dialog>
+        </>,
         document.body
       )}
     </>
